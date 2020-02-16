1b8bc91adf43ea5ea452962a99ba6f37a0e240ea
sakib <sakibakon1@gmail.com>
2020-02-16 22:17:13 +0600

    read me added

--- code_with_database/news-to-n-gram.py
+++ code_with_database/news-to-n-gram.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[33]:
+# In[4]:
 
 
 import xlrd
@@ -17,14 +17,18 @@ for i in range(database.nrows):
     key =database.cell(i, 0).value
     co =int(database.cell(i, 1).value)
     single_dictionary[key]=co
+#os.remove("single_database.xls")
 
+#rb=xlrd.open_workbook("double_database.xls")
 database=rb.sheet_by_index(1)
 double_dictionary = {}
 for i in range(database.nrows):
     key =database.cell(i, 0).value
     co =int(database.cell(i, 1).value)
     double_dictionary[key]=co
+#os.remove("double_database.xls")
 
+#rb=xlrd.open_workbook("triple_database.xls")
 database=rb.sheet_by_index(2)
 triple_dictionary = {}
 for i in range(database.nrows):
@@ -32,30 +36,31 @@ for i in range(database.nrows):
     co =int(database.cell(i, 1).value)
     triple_dictionary[key]=co
 
-# store already processed news
+# check already done news
 database=rb.sheet_by_index(3)
 already_done_dictionary = {}
 for i in range(database.nrows):
     key =database.cell(i, 0).value
     co =int(database.cell(i, 1).value)
     already_done_dictionary[key]=co
-os.remove("database.xls")
 
 
-# In[34]:
+# In[5]:
 
 
-read=xlrd.open_workbook("collected_news.xls")
-read_news=read.sheet_by_name("first")
+read=xlrd.open_workbook("Prothom_Alo.xls")
+read_news=read.sheet_by_index(0)
+import re
 for i in range(read_news.nrows):
-    news=read_news.cell(i,2).value
-    check = read_news.cell(i,1).value
+    news=read_news.cell(i,1).value
+    check = read_news.cell(i,0).value
     #if news is already process then continue
     if check in already_done_dictionary:
         continue;
     already_done_dictionary[check]=1;
     #make three list with one, two and three words.
-    list_with_single_word= news.split()
+    list_with_single_word= re.split('\s|\)|\(|:|\।|\।|, ', news);
+    list_with_single_word= list(filter(None, list_with_single_word))
     list_with_two_word=[]
     list_with_three_word=[]
     for j in range(1, len(list_with_single_word) ):
@@ -89,7 +94,7 @@ for i in range(read_news.nrows):
         
 
 
-# In[35]:
+# In[6]:
 
 
 #store into a database
@@ -98,26 +103,45 @@ write_single = write.add_sheet("single")
 write_double = write.add_sheet("double")
 write_triple = write.add_sheet("triple")
 write_already_done = write.add_sheet("already_done")
-j=0
-for key, cnt in single_dictionary.items(): 
-    write_single.write(j, 0, key)
-    write_single.write(j, 1, cnt)
-    j=j+1
-j=0
-for key, cnt in double_dictionary.items(): 
-    write_double.write(j, 0, key)
-    write_double.write(j, 1, cnt)
-    j=j+1
-j=0
-for key, cnt in triple_dictionary.items(): 
-    write_triple.write(j, 0, key)
-    write_triple.write(j, 1, cnt)
-    j=j+1
-j=0
+row=0
+column=0
+for key, cnt in single_dictionary.items():
+    if(row>65530):
+        row=0
+        column=column+2
+    write_single.write(row, column, key)
+    write_single.write(row, column+1, cnt)
+    row=row+1
+row=0
+column=0
+for key, cnt in double_dictionary.items():
+    if(row>65530):
+        row=0
+        column=column+2
+    write_double.write(row, column, key)
+    write_double.write(row, column+1, cnt)
+    row=row+1
+
+row=0
+column=0
+for key, cnt in triple_dictionary.items():
+    if(row>65530):
+        row=0
+        column=column+2
+    write_triple.write(row, column, key)
+    write_triple.write(row, column+1, cnt)
+    row=row+1
+
+row=0
+column=0
 for key, cnt in already_done_dictionary.items(): 
-    write_already_done.write(j, 0, key)
-    write_already_done.write(j, 1, cnt)
-    j=j+1
+    if(row>65530):
+        row=0
+        column=column+2
+    write_already_done.write(row, column, key)
+    write_already_done.write(row, column+1, cnt)
+    row=row+1
+os.remove("database.xls")
 write.save("database.xls")
 
 
